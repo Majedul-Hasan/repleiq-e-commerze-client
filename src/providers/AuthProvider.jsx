9dc88d16c5a1f09abf76a4a8ecf585bf56967527
Majedul Hasan <hasanmajedul@gmail.com>
2023-07-08 16:45:53 +0600

    signOut from firebase

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -25,13 +25,18 @@ const AuthProvider = ({ children }) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
+    const logOut = () => {
+      setLoading(true);
+      return signOut(auth);
+    };
 
-  const authInfo = {
-    user,
-    loading,
-    createUser,
-    signIn,
-  };
+    const authInfo = {
+      user,
+      loading,
+      createUser,
+      signIn,
+      logOut,
+    };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
